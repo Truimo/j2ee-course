55901e5e8531eab824bed7994cfcfe81c2eba445
Truimo <Xiaomo@20mo.cn>
2023-05-23 11:09:48 +0800

    refactor: clean up bookController

--- src/main/java/com/truimo/controller/BookController.java
+++ src/main/java/com/truimo/controller/BookController.java
@@ -1,15 +1,8 @@
 package com.truimo.controller;
 
-import com.truimo.dto.AddBookDto;
-import com.truimo.entity.Book;
 import com.truimo.service.BookService;
-import com.truimo.vo.ResultCode;
-import com.truimo.vo.ResultResponse;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/books")
@@ -21,47 +14,6 @@ public class BookController {
         this.bookService = bookService;
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<ResultResponse<Book>> getBookById(@PathVariable("id") int id) {
-        Book book = bookService.getBookById(id);
-        return ResponseEntity.ok(new ResultResponse<>(ResultCode.QUERY_SUCCESS, book));
-    }
 
-    @PostMapping
-    public ResponseEntity<ResultResponse<String>> addBook(@RequestBody @Validated AddBookDto addBookDto) {
-        Book book = new Book();
-        book.setBook_title(addBookDto.getName());
-        book.setBook_format(addBookDto.getPress());
-        book.setBook_author(addBookDto.getAuthor());
-        int i = bookService.addBook(book);
-        if (i > 0) {
-            return ResponseEntity.ok(new ResultResponse<String>(ResultCode.ADD_SUCCESS));
-        }
-        return ResponseEntity.badRequest().body(new ResultResponse<String>(ResultCode.ADD_FAIL));
-    }
-
-    @GetMapping
-    public ResponseEntity<ResultResponse<List<Book>>> getAllBook() {
-        List<Book> books = bookService.getAllBook();
-        return ResponseEntity.ok(new ResultResponse<>(ResultCode.QUERY_SUCCESS, books));
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<ResultResponse<String>> delBook(@PathVariable("id") int id) {
-        int i = bookService.delBookById(id);
-        if (i > 0) {
-            return ResponseEntity.ok(new ResultResponse<String>(ResultCode.DEL_SUCCESS));
-        }
-        return ResponseEntity.badRequest().body(new ResultResponse<String>(ResultCode.DEL_FAIL));
-    }
-
-    @PutMapping
-    public ResponseEntity<ResultResponse<String>> editbook(@RequestBody Book book) {
-        int i = bookService.editBook(book);
-        if (i > 0) {
-            return ResponseEntity.ok(new ResultResponse<String>(ResultCode.UPDATE_SUCCESS));
-        }
-        return ResponseEntity.badRequest().body(new ResultResponse<String>(ResultCode.UPDATE_FAIL));
-    }
 
 }
