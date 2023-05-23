2c2ca27853edd4bba3bf0c03ae9e00ae076aa39a
Truimo <Xiaomo@20mo.cn>
2023-05-23 11:19:38 +0800

    feat: bookMapper, getBookById, getHotBooks

--- src/main/java/com/truimo/mapper/BookMapper.java
+++ src/main/java/com/truimo/mapper/BookMapper.java
@@ -1,7 +1,12 @@
 package com.truimo.mapper;
 
+import com.truimo.entity.Book;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface BookMapper {
+    Book getBookById(long id);
+    List<Book> getHotBooks();
 }
