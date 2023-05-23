58ac9e68b0ac7460dc1f0dd84cc84c94b33b4684
Truimo <Xiaomo@20mo.cn>
2023-05-23 11:07:54 +0800

    refactor: clean up bookService

--- src/main/java/com/truimo/service/BookService.java
+++ src/main/java/com/truimo/service/BookService.java
@@ -5,10 +5,4 @@ import com.truimo.entity.Book;
 import java.util.List;
 
 public interface BookService {
-    Book getBookById(long id);
-    Book getBookByName(String name);
-    List<Book> getAllBook();
-    int addBook(Book book);
-    int delBookById(long id);
-    int editBook(Book book);
 }
--- src/main/java/com/truimo/service/impl/BookServiceImpl.java
+++ src/main/java/com/truimo/service/impl/BookServiceImpl.java
@@ -13,34 +13,4 @@ public class BookServiceImpl implements BookService {
 
     @Resource
     private BookMapper bookMapper;
-
-    @Override
-    public Book getBookById(long id) {
-        return bookMapper.getBookById(id);
-    }
-
-    @Override
-    public Book getBookByName(String name) {
-        return bookMapper.getBookByName(name);
-    }
-
-    @Override
-    public List<Book> getAllBook() {
-        return bookMapper.getAllBook();
-    }
-
-    @Override
-    public int addBook(Book book) {
-        return bookMapper.addBook(book);
-    }
-
-    @Override
-    public int delBookById(long id) {
-        return bookMapper.delBookById(id);
-    }
-
-    @Override
-    public int editBook(Book book) {
-        return bookMapper.editBookBySet(book);
-    }
 }
